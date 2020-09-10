82ae07d69fe8f480f67e6c6585fe68ab2d3220ba
markowskid <dmarkowski@itsmgroup.pl>
2020-09-10 21:08:00 +0200

    2020.09.10

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 'use strict';
-var http = require('http');
+const express=require('express')
+const bodyParser=require('body-parser')
+const cookieParser=require('cookie-parser')
+
+const middleware=require('./middleware')
+const api=require('./api')
+const auth=require('./auth')
+
+const app=express();
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+app.use(middleware.cors)
+app.use(bodyParser.json)
+app.use(cookieParser())
+
+app.post('/login',auth.authenticate, auth.login)
+
+app.use(middleware.handleValidationError)
+app.use(middleware.handleError)
+app.use(middleware.notFound)
+
+const server=app.listen(port, ()=>console.log('Server listening on port ${port}'))
